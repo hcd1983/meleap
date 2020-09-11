beb1435cb0481c2defb81445a8c961181d27f2ea
hcd1983 <hcd@mojopot.com>
2020-09-11 08:53:25 +0800

    smaller tilte, margin-bottom for mobile screen

--- resources/views/components/member.blade.php
+++ resources/views/components/member.blade.php
@@ -1,8 +1,17 @@
 <div class="member {{$class}}">
     <div class="photo" style="background-image: url({{$photo}})"></div>
     <div class="data">
-        <h3>{{$name}}</h3>
+        <h3>
+            @if(mb_strlen($name) >= 17)
+                <small class="smaller">{{$name}}</small>
+            @elseif(mb_strlen($name) >= 15)
+                <small >{{$name}}</small>
+            @else
+                {{$name}}
+            @endif
+
+        </h3>
         <div class="title poppins">{{$position}}</div>
-        <div class="team poppins">Team</div>
+{{--        <div class="team poppins">Team</div>--}}
     </div>
 </div>
